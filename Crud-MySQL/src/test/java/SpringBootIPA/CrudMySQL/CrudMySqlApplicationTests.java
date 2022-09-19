52dde4d6edd2bb1d395bf75a9fcd87f37b66efe9
trankhoadev <trankhoadev@gmail.com>
2022-09-19 10:14:18 +0700

    Add blank project with no config

---
+++ Crud-MySQL/src/test/java/SpringBootIPA/CrudMySQL/CrudMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootIPA.CrudMySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
